a9bfc6012836dbccd33f14ea00215c66985bab9c
navig8or <shinbox@live.com>
2021-06-18 21:52:27 -0400

    removed null class selectors in validate js

--- validate.js
+++ validate.js
@@ -55,23 +55,17 @@
       }
     })
     .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
+     
       if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
+        console.log("AOK");
         thisForm.reset(); 
       } else {
         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
       }
     })
     .catch((error) => {
-      displayError(thisForm, error);
+      console.log("some damn error happened!", error);
     });
-  }
-
-  function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
 })();
